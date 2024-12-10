08f88fa010819d1dc2e022070b488ce94b245eb2
Frank Guglielmo <frankieg1610@gmail.com>
2024-12-09 23:42:24 -0800

    Initial Commit

---
+++ Spot/SpotApp.swift
@@ -0,0 +1,20 @@
+//
+//  SpotApp.swift
+//  Spot
+//
+//  Created by Frank Guglielmo on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
